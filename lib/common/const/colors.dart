3d083df0e95ce118a1600a2f71bd651b868183b1
hyeminililo <143622050+hyeminililo@users.noreply.github.com>
2024-08-07 20:55:16 +0900

    feat : 로그인 UI 구현
    로그인 UI 구현

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,10 @@
+// 주색상
+import 'dart:ui';
+
+const PRIMARY_COLOR = Color(0xFF22A45D);
+// 글자 색상
+const BODY_TEXT_COLOR = Color(0xFF868686);
+// 텍스트필드 배경 색상
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+// 텍스트필드 테두리 색상
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
